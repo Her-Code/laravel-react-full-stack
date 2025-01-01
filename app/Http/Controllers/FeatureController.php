b8f4ed0211a09abd2fe593d8590c99ada2b0659f
+Sharon <+wanjiku.kahira@gmail.com>
2025-01-01 23:55:19 +0300

    Modify code in file

--- app/Http/Controllers/FeatureController.php
+++ app/Http/Controllers/FeatureController.php
@@ -26,7 +26,7 @@ class FeatureController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Feature/Create');
     }
 
     /**
@@ -34,7 +34,19 @@ class FeatureController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'name' => ['required','string'],
+            'description' => ['nullable','string'],
+        ]);
+
+        $data['user_id'] = auth()->id();
+
+        Feature::create($data);
+
+        return to_route('feature.index')->with('success','feature created sucessfully');
+
+
+
     }
 
     /**
@@ -42,7 +54,8 @@ class FeatureController extends Controller
      */
     public function show(Feature $feature)
     {
-        //
+        return Inertia::render('Feature/Show', ['feature' => new FeatureResource($feature)
+    ]);
     }
 
     /**
@@ -50,7 +63,8 @@ class FeatureController extends Controller
      */
     public function edit(Feature $feature)
     {
-        //
+        return Inertia::render('Feature/Edit', ['Feature' => new FeatureResource($feature)
+    ]);
     }
 
     /**
@@ -58,7 +72,15 @@ class FeatureController extends Controller
      */
     public function update(Request $request, Feature $feature)
     {
-        //
+        $data = $request->validate([
+            'name' => ['required','string'],
+            'description' =>  ['nullable','string'],
+
+        ]);
+
+        $feature->update($data);
+
+        return to_route('feature.index')->with('success','Feature updated sucessfully');
     }
 
     /**
@@ -66,6 +88,8 @@ class FeatureController extends Controller
      */
     public function destroy(Feature $feature)
     {
-        //
+        $feature->delete();
+
+        return to_route('feature.index')->with('success','Feature deleted successfully');
     }
 }
