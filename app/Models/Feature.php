b9a1aa4ab059db02e8cf2a1a35df28a8149d374a
+Sharon <+wanjiku.kahira@gmail.com>
2025-01-01 19:50:05 +0300

    Modified feature model

--- app/Models/Feature.php
+++ app/Models/Feature.php
@@ -6,8 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory; 
+
 class Feature extends Model
 {
+    use HasFactory;
     public function upvotes():HasMany
     {
         return $this->hasMany(Upvote::class);
