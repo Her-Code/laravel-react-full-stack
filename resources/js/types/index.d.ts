d34f5345e46293a1ac46236c8c39cb8a994acf7d
+Sharon <+wanjiku.kahira@gmail.com>
2025-01-03 16:43:43 +0300

    Modified file

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -22,6 +22,9 @@ export type Feature = {
     description: string;
     user:User;
     created_at: string;
+    upvote_count:number;
+    user_has_upvoted:boolean;
+    user_has_downvoted:boolean;
 }
 
 export type PageProps<
